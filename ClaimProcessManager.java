0f287b201d3988671e0c3ef8e4506f804f8008b2
ThePham10 <s3981133@rmit.edu.vn>
2024-04-03 02:08:38 +0700

    DependentList Class

--- ClaimProcessManager.java
+++ ClaimProcessManager.java
@@ -2,12 +2,8 @@ import java.util.List;
 
 public interface ClaimProcessManager {
     public boolean addClaim(Claim claim);
-
     public boolean updateClaim(String claimID);
-
     public void deleteClaim(String claimID);
-
     public Claim getOneClaim(String claimID);
-
     public List<Claim> getAllClaims();
 }
--- DependentList.java
+++ DependentList.java
@@ -2,10 +2,7 @@ import java.util.List;
 
 public interface DependentList {
     public boolean addDependent(Customer dependent);
-
     public void deleteDependent(String customerID);
-
     public Customer getOneDependent(String dependentID);
-
     public List<Customer> getAllDependents();
 }
